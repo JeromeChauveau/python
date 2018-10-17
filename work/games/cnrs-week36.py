701b5ff21d00fcffc5edc9b333518b5de52a9624
JeromeChauveau <39281271+JeromeChauveau@users.noreply.github.com>
2018-10-17 05:35:47 +0200

    Create cnrs-week36.py

---
+++ work/games/cnrs-week36.py
@@ -0,0 +1,19 @@
+
+
+'''
+http://images.math.cnrs.fr/Septembre-2017-2e-defi.html*
+Combien y a-t-il de nombres à dix chiffres tous différents abcdefghij tels que a+j=b+i=c+h=d+g=e+f=9 ?
+
+
+Algo:
+
+Identifier le nombre n de couples de chiffres dont la somme est neuf, sachant que l'ordre compte (90 et 09 par exemple sont valables)
+Note : cela peut être élargi à la recherche du nombre de n-uplet dont la somme vaut m
+Identifier le nombre de façons de choisir 4 couples parmi les n trouvés, sachant que le même couple peut être choisi plusieurs fois
+Note : il s'agit du nombre de tirages avec remise. 
+Attention néanmoins: 
+ - l'ordre a son importance (9900 et 0099 sont différents)
+ - il ne faut pas compte les doublons
+
+
+'''
