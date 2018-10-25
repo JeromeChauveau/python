1091436cbee424a2635337f0c348a0f549b7478f
JeromeChauveau <39281271+JeromeChauveau@users.noreply.github.com>
2018-10-25 23:18:49 +0200

    Update test.py

--- work/games/test.py
+++ work/games/test.py
@@ -1,3 +1,18 @@
-// Test de la fonction qui renvoie le nombre de nombres à n chiffres dont tels que la somme deux à deux des chiffres les composant 
-// (1+n, 2+ n-1...) soit égal à un nombre
-/ 
+/* Test de la fonction qui renvoie le nombre X de nombres à n chiffres dont tels que la somme deux à deux des chiffres les composant 
+ (1+n, 2+ n-1...) soit égal à un nombre Y
+ On pourrait aussi introduire une notion d''ordre (par exemple, la somme des nombres qui se suivent : pour un nombre abc
+
+
+/* Quelles sont les paramètres de la fonction? 
+ 1- Le nombre Y de chiffres des nombres à compter
+ 2- Le nombre X
+--> f (nombreATrouver, tailleNombres)
+
+Si tailleNombres est impair, f renvoie une erreur
+Si d
+                                                   
+                                                   
+
+                                                   
+                                                   
+                                                   
